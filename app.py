4ab021f4e184b3540cf7ef706ca7bdd30e290341
gudisela <40882825+gudisela@users.noreply.github.com>
2026-02-23 23:14:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -402,10 +402,6 @@ def student_review(exam_id, student):
 # RUN
 # ---------------------------------------
 
-if __name__ == "__main__":
-    import os
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
 
 
